ca2eb798e35e91d5667251a78fc59aea16bb3fe8
Prince <pd9661037@gmail.com>
2023-07-31 21:09:31 +0530

    changes app.js

--- app.js
+++ app.js
@@ -16,18 +16,6 @@ app.use(express.json())
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 
-// Your code
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  
-  app.use(express.static(path.resolve(__dirname, 'client/build')));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-  })
-}
-// Your code
-
-
 app.listen(port, () => {
   console.log(`Server is working on http://localhost:${port}`)
 })
